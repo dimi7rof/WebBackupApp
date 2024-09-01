8850d1191604821c0c8839ded3944bf15fc5dc3a
dimi7 <dimi7rof@gmail.com>
2024-09-01 18:02:21 +0300

    Refactor

--- WebBackUp/Endpoints/Backup.cs
+++ WebBackUp/Endpoints/Backup.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Text.Json;
 
 namespace WebBackUp.Endpoints;
 
@@ -11,7 +12,7 @@ internal static class Backup
             "set1" or "set4" or "set5" or "set6" => Phone.Execute,
             "set2" => Hdd.Execute,
             "set3" => SdCard.Execute,
-            _ => Hdd.Execute // Default case
+            _ => Hdd.Execute
         };
 
         await hubContext.Clients.All.SendAsync("ReceiveProgress", "\n");
@@ -25,4 +26,48 @@ internal static class Backup
 
         return Results.Ok();
     }
+
+    internal static IResult Save(PathData pathData, string setId)
+    {
+        var filePath = GetFilePath(setId);
+        var jsonData = JsonSerializer.Serialize(pathData);
+        File.WriteAllText(filePath, jsonData);
+
+        var setName = GetSetName(setId);
+        return Results.Json(new { Message = $"Input values saved successfully for {setName}." });
+    }
+
+    internal static IResult Load(string setId)
+    {
+        var filePath = GetFilePath(setId);
+        if (!File.Exists(filePath))
+        {
+            return Results.Json(new { SourcePaths = new List<string>(), DestinationPaths = new List<string>() });
+        }
+
+        var jsonData = File.ReadAllText(filePath);
+        var pathData = JsonSerializer.Deserialize<PathData>(jsonData);
+
+        return Results.Json(new
+        {
+            SourcePaths = pathData?.SourcePaths ?? [],
+            DestinationPaths = pathData?.DestinationPaths ?? []
+        });
+    }
+
+    private static string GetFilePath(string setId) => $"{setId}_paths.json";
+
+    private static string GetSetName(string setId) => setId switch
+    {
+        "set1" => "Phone",
+        "set2" => "HDD",
+        "set3" => "SD Card",
+        "set4" => "Phone 2",
+        "set5" => "Phone 3",
+        "set6" => "Phone 4",
+        "set7" => "HDD 2",
+        "set8" => "HDD 3",
+        "set9" => "HDD 4",
+        _ => "Unknown"
+    };
 }
