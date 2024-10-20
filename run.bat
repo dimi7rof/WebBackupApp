3d8c01148587d6698220ba104b6523b6336371cd
dimi7 <dimi7rof@gmail.com>
2024-10-20 13:57:52 +0300

    Update: run.bat

--- run.bat
+++ run.bat
@@ -1,10 +1,10 @@
 @echo off
 :: Navigate to the Backend folder and run WebBackUp.exe
-cd Backend\WebBackUp\bin\Release\net8.0
+cd service\src\WebBackUp\bin\Release\net8.0
 start "" "WebBackUp.exe"
 
 :: Navigate back to the root directory and then to the Frontend folder to start the Angular app
-cd ..\..\..\..\..\Frontend\backup-app
+cd ..\..\..\..\..\..\application\backup-app
 start "" "cmd /c ng serve"
 
 timeout /t 10 /nobreak
